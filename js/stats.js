4ddadde74dad798936c53ab1cb792cd0944c4940
tomasAybar <aybar.t4@gmail.com>
2022-05-28 11:02:04 -0300

    tabla Events Statistics funcionando

--- js/stats.js
+++ js/stats.js
@@ -28,7 +28,51 @@ async function getFetch() {
 
                 // console.log(arraySortedCapacity[arraySortedCapacity.length - 1]) // mayor capacidad
 
+                let arrayTableEvents = [];
+
+                arrayTableEvents.push( arraySortedPast[arraySortedPast.length - 1] );
+                arrayTableEvents.push( arraySortedPast[0] )
+                arrayTableEvents.push( arraySortedCapacity[arraySortedCapacity.length - 1] )
+
+                console.log(arrayTableEvents);
+
+                printTableEvents(arrayTableEvents, tableEvents);
 
             })
 }
 
+function printTableEvents(arrayDatos, contenedorTable) {
+
+    let resultado = []
+
+    arrayDatos.forEach(evento => {
+
+        if (evento.capacity) {
+
+            // let mayorCapacity = `${evento.name}: ${evento.capacity}`;
+
+            resultado.push(`${evento.name}: ${evento.capacity}`);
+
+        } else {
+
+            resultado.push(`${evento.name} with %${evento.percentage} of assistance`)
+            // percentage.push(evento)
+        }
+    })
+
+    // console.log(resultado);
+
+    let contenedorFilas = document.createElement('tr');
+
+    resultado.forEach(elemento => {
+
+        let filaElmento = document.createElement('td');
+
+        filaElmento.classList.add('p-2');
+        filaElmento.innerHTML = elemento;
+        contenedorFilas.appendChild(filaElmento);
+
+    })
+
+    contenedorTable.appendChild(contenedorFilas);
+}
